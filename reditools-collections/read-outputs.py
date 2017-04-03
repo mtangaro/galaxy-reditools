9c0c74fc1b957ae9edbcd2c45da7b535b96b12d5
mtangaro <ma.tangaro@gmail.com>
2017-04-03 14:20:13 +0000

    fix html

--- reditools-collections/read-outputs.py
+++ reditools-collections/read-outputs.py
@@ -1,18 +1,3 @@
-#RESULTS=$1
-#BASEPATH= $2
-#
-#echo "<html><body><table border=1>"
-#echo '<a href=${RESULTS}>ciao</>'
-#echo "$RESULTS" | sed -r "s|$BASEPATH(.*)|<a href=\"\\1\">\\1</a>|" | sed '
-#i<tr><td>
-#s|\t|</td><td>|g
-#a<\/td><\/tr>
-#'
-#echo "<p>"
-#echo "</table></body></html>"
-
-
-
 '''
 comments here
 '''
@@ -20,37 +5,100 @@ comments here
 import os
 import argparse
 
+import random
+pid=str(os.getpid()+random.randint(0,999999999))
 
 def parse_cli_options():
   parser = argparse.ArgumentParser(description='Read reditools data tables', formatter_class=argparse.RawTextHelpFormatter)
   parser.add_argument( '-i', dest='result', help='')
   parser.add_argument( '-p', dest='path', help='')
-  parser.add_argument( '-o', dest='output directory', help='')
-  parser.add_argument( '-s', dest='sample id', help='')
-  parser.add_argument( '-g', dest='group id', help='')
-  
+  parser.add_argument( '-o', dest='output', help='')
+  parser.add_argument( '-d', dest='outdir', help='')
+  parser.add_argument( '-s', dest='sample_id', help='')
+  parser.add_argument( '-g', dest='group_id', help='')
+  parser.add_argument( '-j', '--inputfilename', dest='input_filename', help='')
+  parser.add_argument( '-t', '--init_html', dest='init_html', action='store_true', help='')
+  parser.add_argument( '-c', '--close_html', dest='close_html', action='store_true', help='')
+ 
   return parser.parse_args()
 
-
-
-def create_html():
+def init_html_file(fout):
   '''
   Create html file
   '''
+  f = file(fout,'w')
   html = []
+
+  # Start html code
   html.append('<html><body><table border=1>')
+  html.append('<tr><td>')
+  html.append('RNA BAM file</td><td>DNA BAM file</td><td>ID sample</td><td>ID group</td><td>ID output')
+  html.append('</td></tr>')
+
+  f.write('\n'.join(html))
+  f.write('\n')
+  f.close()
+  return f
+
+def close_html_file(fout):
+  '''
+  Create html file
+  '''
+  f = file(fout,'a')
+  html = []
+
+  # Close html code
+  html.append('<p>')
+  html.append('</table></body></html>')
+
+  f.write('\n'.join(html))
+  f.write('\n')
+  f.close()
+  return f
+
+
+def create_out_dir(directory):
+  if not os.path.exists(directory):
+    os.makedirs(directory)
+
+def create_output_symlink(source, linkname):
+  os.symlink(source, linkname)
+
+def update_html(ref,basepath):
+  '''
+  Update output html file
+  '''
+  html = []
+  html.append('<tr><td>')
+  html.append('RNA BAM file</td><td>DNA BAM file</td><td>ID sample</td><td>ID group</td><td><a href="%s">%s</a>' % (ref+'.txt',basepath))
+  html.append('</td></tr>')
   return html
 
 
 #def create_tab_list_file():
 
 def main():
-  print 'ciao';
-  fhtml = create_html
-  print fhtml
-  fin = file('index.html','w')
+  options = parse_cli_options()
+
+  if(options.init_html):
+    init_html_file(options.output)
+    return
+
+  if(options.close_html):
+    close_html_file(options.output)
+    return
+
+  create_out_dir(options.outdir)
+
+  filename = 'outtable_' + pid
+  filepath = str(options.outdir) + '/' + filename + '.txt'
+
+  create_output_symlink(options.result, filepath)
+
+  fhtml = update_html(filename, filename)
+  fin = file(options.output,'a')
   fin.write('\n'.join(fhtml))
-  #fin.write('\n')
+  fin.write('\n')
   fin.close()
 
 if __name__=="__main__":
